cac8e0d6dc949deeaa2f7c5126339bb93a03e1ad
xiseevi <59847880+xiseevi@users.noreply.github.com>
2023-09-11 18:25:17 +0300

    Update Фибоначчи.py

--- Фибоначчи.py
+++ Фибоначчи.py
@@ -1,22 +1,30 @@
-﻿# Fibonacci v0.1
+# Fibonacci v0.5
 # Made by Mahmduov A.V., group 4305, SPB GTI(TU).
-print('Фибоаччи v0.1')
+print('Фибоаччи v0.5')
 
-print('Введите любое число')
-def isfib(number):
-    num1 = 1
-    num2 = 1
+def isfib():
+# def кастомная функция
+# number - переменная которую мы вводим.
     while True:
-        if num2 <= number:
+    # While - это цикл. Выполняет тело цикла до тех пор пока условия цикла истина. в данном случае цикл проверки на фибоначи до тех пока не будет введен любой не численный символ.
+        try:
+            number = int(input("Введите число (или не число для выхода): "))
+        except ValueError:
+            print("Выход")
+            break
+        # break - оператор который досрочно прерывает цикл.
+
+        num1 = 1
+        num2 = 1
+        while num2 <= number:
             if num2 == number:
-                return 'Фибоначчи'
+                print('Фибоначчи')
+                break
             else:
                 tempnum = num2
                 num2 += num1
                 num1 = tempnum
-                
         else:
-            return 'Не Фибоначчи'
-                
-print(isfib(int(input())))
-g = int(input('<=== To be continued...'))
+            print('Не Фибоначчи')
+        # Идет проверка на число фибонначи до тех пока оно не будет определенно как число фибонначи или нет.
+isfib()
